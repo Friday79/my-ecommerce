6c9914a7da194af0445a1b4b58db0a3bf863b480
Friday79 <fuwuigbe@gmail.com>
2026-08-29 17:51:27 +0000

    Category change to categories

--- backend/store/models.py
+++ backend/store/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
+    class Meta:
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.name
@@ -11,7 +13,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price= models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='products/')
     stock = models.PositiveIntegerField()
